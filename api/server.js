4f6c9480002afd98ca0e6d58fb7dec3d0d69158d
stanleyyellowzx <stanleyhuang0702@gmail.com>
2025-05-24 16:22:13 -0400

    removed unnecessary imports

--- api/server.js
+++ api/server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const pokedb = require("./database.js");
-
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+
 
 app.get('/', (req, res) => {
     res.send("hello world");
